54f9766fcab53219859917e424d4c5f6ae0fa5e8
Shrey Shah <shreyss99@gmail.com>
2021-09-19 19:30:08 +0530

    Updated secret removal

--- Full Stack Coursework/Module - Python/django_project/django_project/settings.py
+++ Full Stack Coursework/Module - Python/django_project/django_project/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n9k!^^-j8ntmgg%g%_^181tun9!ef)as2=te7vuwj^9_(js8wu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
