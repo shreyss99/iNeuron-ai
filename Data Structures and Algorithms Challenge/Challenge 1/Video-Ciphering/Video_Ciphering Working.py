35fbbd01e6a569d0ee89e109585ed498342a975a
Shrey Shah <shreyss99@gmail.com>
2022-03-04 19:57:37 +0530

    Added Start Button and functionality

--- Data Structures and Algorithms Challenge/Challenge 1/Video-Ciphering/Video_Ciphering Working.py
+++ Data Structures and Algorithms Challenge/Challenge 1/Video-Ciphering/Video_Ciphering Working.py
@@ -22,3 +22,24 @@ from moviepy.editor import *
 window = tk.Tk() # created a tkinter gui window frame
 window.title("Video Ciphering") # title for the window frame
 window.geometry('1000x700') # window size
+
+
+# Top label
+
+start1 = tk.Label(text = "VIDEO  CIPHERING", font=("Arial", 55,"underline"), fg="black")
+start1.place(x = 120, y = 10)
+
+
+# Defining the start function
+# The current window should be killed when user chooses START
+
+def startFunction():
+    window.destroy()
+
+
+# START button created and assigned the functionality
+
+startb = Button(window, text="START",command=start_fun,font=("Arial", 25), bg = "green", fg = "black", borderwidth=3, relief="raised")
+startb.place(x = 150, y = 600 )
+
+
