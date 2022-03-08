1234ee65fc13b4ddea8c7f829d936ece7baacf02
Shrey Shah <shreyss99@gmail.com>
2022-03-09 02:05:24 +0530

    Aligned comments

--- Data Structures and Algorithms Challenge/Challenge 1/Video-Ciphering/Code/Video_Ciphering Working.py
+++ Data Structures and Algorithms Challenge/Challenge 1/Video-Ciphering/Code/Video_Ciphering Working.py
@@ -192,10 +192,10 @@ def encrypt_fun():
         ret, frame = cap.read()
         if ret == True:
 
-        # Display the resulting frame
+            # Display the resulting frame
             cv2.imshow('Encrypted Video', frame)
 
-        # Press Q on keyboard to exit
+            # Press Q on keyboard to exit
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
@@ -220,10 +220,10 @@ def decrypt_fun():
         ret, frame = cap.read()
         if ret == True:
 
-        # Display the resulting frame
+            # Display the resulting frame
             cv2.imshow('Decryptedted Video', frame)
 
-        # Press Q on keyboard to exit
+            # Press Q on keyboard to exit
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
@@ -231,6 +231,9 @@ def decrypt_fun():
             
     cap.release()
     cv2.destroyAllWindows()
+    
+    
+
                  
 
                 
