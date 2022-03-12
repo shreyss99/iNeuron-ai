93011808abc9f02cfb4e88451c7974b4dd847352
Shrey Shah <shreyss99@gmail.com>
2022-03-12 23:23:51 +0530

    Store output file details

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/zip/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/zip/compressor.py
@@ -5,7 +5,11 @@ import os
 def compress(input_file, output_path):
 
     # Compress input_file, store it in output_path and then return output_file
-    # Get file name and extension
     
+    # Get file name and extension
     input_filename, input_fileext = os.path.splitext(os.path.basename(input_file))
+    
+    # Output file name and path
+    output_filename = input_filename + '.zip'
+    output_file = os.path.join(output_path, output_filename)
     
