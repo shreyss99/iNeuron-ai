649dddb0d4b817e58a46f165e5201ec68b9c944d
Shrey Shah <shreyss99@gmail.com>
2022-03-12 23:22:25 +0530

    Extract file name and extension

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/zip/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/zip/compressor.py
@@ -1,3 +1,11 @@
 import zipfile
 import io
 import os
+
+def compress(input_file, output_path):
+
+    # Compress input_file, store it in output_path and then return output_file
+    # Get file name and extension
+    
+    input_filename, input_fileext = os.path.splitext(os.path.basename(input_file))
+    
