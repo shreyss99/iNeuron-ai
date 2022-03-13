d993dccbb69b0dc2aa03e16fde74718895e9b2aa
Shrey Shah <shreyss99@gmail.com>
2022-03-13 23:41:23 +0530

    Store output file name and path

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/huffman/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/huffman/compressor.py
@@ -11,3 +11,7 @@ def compress(input_file, output_path):
     
     # Get input file name and extension
     input_filename, input_fileext = os.path.splitext(os.path.basename(input_file))
+    
+    # Store output file name and path
+    output_filename = input_filename + '.txt'
+    output_file = os.path.join(output_path, output_filename)
