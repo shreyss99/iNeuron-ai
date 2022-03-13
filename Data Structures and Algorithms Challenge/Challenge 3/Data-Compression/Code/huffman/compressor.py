df555f5d068059322cd649417da9b3f53cd1a322
Shrey Shah <shreyss99@gmail.com>
2022-03-13 23:40:22 +0530

    Get input file name and extension

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/huffman/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/huffman/compressor.py
@@ -8,3 +8,6 @@ import pickle
 def compress(input_file, output_path):
     ''' Compress input_file, store it in output_path and then
     return output_file '''
+    
+    # Get input file name and extension
+    input_filename, input_fileext = os.path.splitext(os.path.basename(input_file))
