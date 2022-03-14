4cc5bf57ad1fd0bbef3b414e6bea326af985b3bd
Shrey Shah <shreyss99@gmail.com>
2022-03-14 21:57:35 +0530

    Store input and output file details

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/lzw/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/lzw/compressor.py
@@ -2,6 +2,15 @@ import os
 
 def compress(input_file, output_path):
 	'''Get input file name and extension'''
+    
+    # Get input file name and extension
     input_filename, input_fileext = os.path.splitext(os.path.basename(input_file))
+    
+    # Store output file name and path
     output_filename = input_filename + '.txt'
     output_file = os.path.join(output_path, output_filename)
+    
+    with open(input_file, 'r') as f:
+        data = f.read()
+
+    codes = encode(data)
