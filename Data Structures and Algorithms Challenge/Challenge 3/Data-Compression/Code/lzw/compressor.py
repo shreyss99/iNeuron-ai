2147251b5526825d671a4b5e731f3adf980c34df
Shrey Shah <shreyss99@gmail.com>
2022-03-15 22:09:21 +0530

    Updated code

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/lzw/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/lzw/compressor.py
@@ -1,13 +1,15 @@
 import os
 
 def compress(input_file, output_path):
-	'''Get input file name and extension'''
+    '''Get input file name and extension'''
     
     # Get input file name and extension
     input_filename, input_fileext = os.path.splitext(os.path.basename(input_file))
+        
+def compress(input_file, output_path):
     
     # Store output file name and path
-    output_filename = input_filename + '.txt'
+    output_filename = 'Compressed_' + input_filename + '.txt'
     output_file = os.path.join(output_path, output_filename)
     
     with open(input_file, 'r') as f:
@@ -23,7 +25,7 @@ def compress(input_file, output_path):
     return output_file
     
     
-    def encode(data):
+def encode(data):
     
     # Initialize dictionary
     dic = {chr(c): c for c in range(0, 256)}
