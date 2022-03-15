55ba9abbd95d299b66cf31b348517b63949652d8
Shrey Shah <shreyss99@gmail.com>
2022-03-15 22:56:19 +0530

    Updated image name

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/lzw/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/lzw/compressor.py
@@ -7,7 +7,7 @@ def compress(input_file, output_path):
     input_filename, input_fileext = os.path.splitext(os.path.basename(input_file))
     
     # Store output file name and path
-    output_filename = 'Compressed_' + input_filename + '.txt'
+    output_filename = 'Compressed_LZW_' + input_filename + '.txt'
     output_file = os.path.join(output_path, output_filename)
     
     with open(input_file, 'r') as f:
