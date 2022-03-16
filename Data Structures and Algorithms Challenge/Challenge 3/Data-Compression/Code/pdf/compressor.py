b23f05c1f47169060df46fd4f3c521f90ea54741
Shrey Shah <shreyss99@gmail.com>
2022-03-17 03:58:40 +0530

    Define compression function and get input file detail

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/pdf/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/pdf/compressor.py
@@ -5,3 +5,10 @@ import glob
 
 # public key
 global public_key
+
+
+def compress(input_file, output_path):
+    ''' Compress input_file, store it in output_path and then return output_file '''
+    
+    # Get file name and extension
+    input_filename, input_fileext = os.path.splitext(os.path.basename(input_file))
