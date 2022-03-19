562eff2c0480e839861560b5e3a713a513914b27
Shrey Shah <shreyss99@gmail.com>
2022-03-19 17:47:34 +0530

    Added equal and not-equal functions

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/entity/node.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/entity/node.py
@@ -13,7 +13,22 @@ class Node(object):
         return self.left is None and self.right is None
 
 
-    def __lt__(self, other):
+    def __lt__(self, temp):
         ''' Sorting criteria for inserting into priority queue '''
         
-        return (self.freq < other.freq)
+        return (self.freq < temp.freq)
+        
+    
+    def __eq__(self, temp):
+        if not isinstance(self, temp.__class__):
+            return False
+            
+        return self.char == temp.char and self.freq == temp.freq
+
+
+    def __ne__(self, temp):
+        return not self.__eq__(temp)
+
+
+    def __str__(self):
+        return '({} : {})'.format(self.char, self.freq)
