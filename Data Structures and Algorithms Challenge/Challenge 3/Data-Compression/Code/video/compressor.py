3dde2c6fa039013d45ddbff4271d833cb98aba35
Shrey Shah <shreyss99@gmail.com>
2022-03-19 21:54:23 +0530

    Input and output file details

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/video/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/video/compressor.py
@@ -4,5 +4,12 @@ import os
 
 
 def compress(input_file, output_path):
-
+'''Get input file name and extension'''
+    
+    # Get input file name and extension
+    input_filename, input_fileext = os.path.splitext(os.path.basename(input_file))
+    
+    # Store output file name and path
+    output_filename = 'Compressed_' + input_filename + '.mp4'
+    output_file = os.path.join(output_path, output_filename)
  
