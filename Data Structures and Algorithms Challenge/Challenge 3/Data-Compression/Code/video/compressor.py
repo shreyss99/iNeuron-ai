784320a4e11de34334dc2b44cee8c5e12703ae05
Shrey Shah <shreyss99@gmail.com>
2022-03-19 21:55:43 +0530

    Get video and store dimensions

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/video/compressor.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/Code/video/compressor.py
@@ -13,3 +13,9 @@ def compress(input_file, output_path):
     output_filename = 'Compressed_' + input_filename + '.mp4'
     output_file = os.path.join(output_path, output_filename)
  
+    # Getting video and then processing it and saving it
+    cap = cv2.VideoCapture(input_file)
+
+    compression_value = 50
+    width  = cap.get(3) * compression_value / 100
+    height = cap.get(4) * compression_value / 100
