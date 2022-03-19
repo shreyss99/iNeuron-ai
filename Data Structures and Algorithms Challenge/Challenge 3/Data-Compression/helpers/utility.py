8cac043bfac2e840179da052c712070c0696d798
Shrey Shah <shreyss99@gmail.com>
2022-03-19 18:10:12 +0530

    Added Padded Encoded string function

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/helpers/utility.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/helpers/utility.py
@@ -7,3 +7,15 @@ def get_encoded_str(root, data):
     for d in data:
         compressed_data.append(codes[d])
     return ''.join(compressed_data)
+    
+    
+def pad_encoded_str(encoded_str):
+
+    ''' Return encoded string with padding zero
+    for storing in file in bytes '''
+    
+    extra_zero = 8 - len(encoded_str) % 8
+    padded_encoded_str = '0' * extra_zero + encoded_str
+    extra_zero_info = '{0:08b}'.format(extra_zero)
+    padded_encoded_str = extra_zero_info + padded_encoded_str
+    return padded_encoded_str
