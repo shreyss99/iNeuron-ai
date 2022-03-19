349b6d1298af388844bf5ee9e9745624bc90edc4
Shrey Shah <shreyss99@gmail.com>
2022-03-19 18:13:42 +0530

    Added codes and assign_codes function

--- Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/helpers/utility.py
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/helpers/utility.py
@@ -19,3 +19,35 @@ def pad_encoded_str(encoded_str):
     extra_zero_info = '{0:08b}'.format(extra_zero)
     padded_encoded_str = extra_zero_info + padded_encoded_str
     return padded_encoded_str
+    
+
+def get_codes(root):
+
+    ''' Return codes for each element in original data '''
+    
+    current = root
+    codes = {}
+    code = []
+
+    _assign_codes(root, codes, code)
+    return codes
+    
+    
+def _assign_codes(current, codes, code):
+
+    ''' Recursively get codes helper '''
+    
+    if current.is_leaf():
+        key = current.char
+        codes[key] = ''.join(code)
+        return
+
+    if current.left:
+        code.append('0')
+        _assign_codes(current.left, codes, code)
+        code.pop()
+
+    if current.right:
+        code.append('1')
+        _assign_codes(current.right, codes, code)
+        code.pop()
