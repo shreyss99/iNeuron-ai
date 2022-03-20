1e9f9ea4b8de47bd71683af632807e46782cade2
Shrey Shah <shreyss99@gmail.com>
2022-03-20 11:17:25 +0530

    Import libraries into driver code

---
+++ Data Structures and Algorithms Challenge/Challenge 3/Data-Compression/compress.py
@@ -0,0 +1,12 @@
+import Code.huffman.compressor
+import Code.lzw.compressor
+import Code.shannon.compressor
+import Code.zip.compressor
+import Code.image.compressor
+import Code.wav_mp3.compressor
+import Code.pdf.compressor
+import Code.video.compressor
+import os
+import argparse
+import sys
+
