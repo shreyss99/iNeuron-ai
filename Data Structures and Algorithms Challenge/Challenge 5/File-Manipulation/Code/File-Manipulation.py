1ed5049f6798a4ad71786fbde70e4cce8e6ff612
Shrey Shah <shreyss99@gmail.com>
2022-03-24 02:58:38 +0530

    Add File Manipulation menu

--- Data Structures and Algorithms Challenge/Challenge 5/File-Manipulation/Code/File-Manipulation.py
+++ Data Structures and Algorithms Challenge/Challenge 5/File-Manipulation/Code/File-Manipulation.py
@@ -4,6 +4,17 @@ import os
 import glob
 
 
+def main():
+    print("Welcome to FILE MANIPULATION project")
+    print()
+    print("--------MENU-------")
+    print("1 - Search a File")
+    print("2 - Sort Files")
+    print("3 - Segregate Files")
+    print("4 - Merge Files")
+    print()
+    
+
 # Define the driver code
 
 if __name__ == "__main__":
