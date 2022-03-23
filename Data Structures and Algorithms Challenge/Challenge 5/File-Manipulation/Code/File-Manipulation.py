e4bfa9d5bf28d3831a3a86b22c8bd33746ddf66b
Shrey Shah <shreyss99@gmail.com>
2022-03-24 04:05:53 +0530

    Add code for extension based or file name based search

--- Data Structures and Algorithms Challenge/Challenge 5/File-Manipulation/Code/File-Manipulation.py
+++ Data Structures and Algorithms Challenge/Challenge 5/File-Manipulation/Code/File-Manipulation.py
@@ -20,6 +20,24 @@ def main():
     option = int(input("Choose one of the above 4 options: "))
     path = input("Enter the path where you want to perform the above operation: ")
     
+    # SEARCH FILE PROCESSING
+    if option == 1:
+        print("Do you want to search by extension or by file name?")
+        print("1 - Search by Extension")
+        print("2 - Search by File Name")
+        criteria = int(input("Choose one of the above 4 options: "))
+        
+        if criteria == 1:
+            extension = input("Enter the file extension: ")
+            result = searchFile(path, extension)
+            
+        elif criteria == 2:
+            searchFileName = input("Enter the name of the file to be searched: ")
+            result = searchFile(path, searchFileName)
+
+
+
+
 
 # Define the driver code
 
