c872f9d252afcf298b0b57bd0164fef8a8e41c81
Shrey Shah <shreyss99@gmail.com>
2022-03-27 23:14:04 +0530

    Importing matrix and recommendation system libraries

--- Data Structures and Algorithms Challenge/Challenge 2/Recommendation-System/Code/Recommendation-System.py
+++ Data Structures and Algorithms Challenge/Challenge 2/Recommendation-System/Code/Recommendation-System.py
@@ -6,3 +6,9 @@ import math
 import re
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Import compressed sparse matrix library for computation 
+from scipy.sparse import csr_matrix
+# Importing SVD from surprise library for building recommender systems
+# SVD is Singular Value Decompostion
+from surprise import Reader, Dataset, SVD
